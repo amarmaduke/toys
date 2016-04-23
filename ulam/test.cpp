1491518a340e3cd3d28adaf41308cfeb9ef0a3a7
Andrew Marmaduke <marmaduke.andrew@gmail.com>
2016-04-22 20:30:19 -0400

    Work on stuff

--- ulam/test.cpp
+++ ulam/test.cpp
@@ -2,33 +2,49 @@
 #include <vector>
 #include <map>
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
+#ifdef _MSC_VER
 #define and &&
 #define not !
+#endif
 
 const int inches = 8;
 
 // Target DPI: 300
-const int DPI = 600;
-const int dotw = 4;
-const int doth = 4;
+const int DPI = 1200;
+const int dotw = 6;
+const int doth = 6;
 
-const int W = inches*DPI - dotw;
-const int H = inches*DPI - doth;
+const int W = inches*DPI;
+const int H = inches*DPI;
 
-const int dots = (W/dotw)*(H/doth);
+const int DOTW = W/dotw - 1;
+const int DOTH = H/doth - 1;
+
+const int dots = DOTW*DOTH;
 
 int image[W][H];
-bool mask[W/dotw][H/doth];
+bool mask[DOTW][DOTH];
 bool primes[dots + 1];
+
+void print_consts()
+{
+   cerr << "inches: " << inches << endl;
+   cerr << "DPI: " << DPI << endl;
+   cerr << "dotw and doth: " << dotw << " " << doth << endl;
+   cerr << "W and H: " << W << " " << H << endl;
+   cerr << "DOTW and DOTH: " << DOTW << " " << DOTH << endl;
+   cerr << "dots: " << dots << endl;
+}
 
 void print_mask()
 {
-   for (int i = 0; i < W/dotw; ++i)
+   for (int i = 0; i < DOTW; ++i)
    {
-      for (int j = 0; j < H/doth; ++j)
+      for (int j = 0; j < DOTH; ++j)
       {
          cout << mask[i][j] << " ";
       }
@@ -104,6 +120,22 @@ int main()
    // Setup
    /* // 10 x 10
    vector<int> dot_data = {
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+      0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+      0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+      0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+      0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0,
+      0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+      0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+      0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0,
+      0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+   };
+   //*/
+   /* // 10 x 10
+   vector<int> dot_data = {
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
       0, 0, 0, 0, 1, 1, 0, 0, 0, 0,
@@ -116,14 +148,14 @@ int main()
       0, 0, 0, 0, 0, 0, 0, 0, 0, 0
    };
    //*/
-   /* // 6 x 6
+   //* // 6 x 6
    vector<int> dot_data = {
-      0, 0, 0, 0, 0, 0,
-      0, 0, 1, 1, 0, 0,
-      0, 1, 1, 1, 1, 0,
-      0, 1, 1, 1, 1, 0,
-      0, 0, 1, 1, 0, 0,
-      0, 0, 0, 0, 0, 0,
+      4, 4, 3, 3, 4, 4,
+      4, 2, 1, 1, 2, 4,
+      3, 1, 0, 0, 1, 3,
+      3, 1, 0, 0, 1, 3,
+      4, 2, 1, 1, 2, 4,
+      4, 4, 3, 3, 4, 4,
    };
    //*/
    /* // 5 x 5
@@ -135,7 +167,7 @@ int main()
       0, 0, 0, 0, 0,
    };
    //*/
-   //* // 4 x 4
+   /* // 4 x 4
    vector<int> dot_data = {
       0, 0, 0, 0,
       0, 1, 1, 0,
@@ -151,22 +183,24 @@ int main()
    }
 
    // PGM Generation
-   cout << "P1" << endl;
+   int white = 4;
+   cout << "P2" << endl;
    cout << W << " " << H << endl;
-   for (int i = 0, dx = 0; i < W; i += dotw, ++dx)
+   cout << white << endl;
+   for (int i = 0, dx = 0; i < W - dotw; i += dotw, ++dx)
    {
-      for (int j = 0, dy = 0; j < H; j += doth, ++dy)
+      for (int j = 0, dy = 0; j < H - doth; j += doth, ++dy)
       {
          for (int x = 0; x < dotw; ++x)
          {
             for (int y = 0; y < doth; ++y)
             {
-               int I = i + x;
-               int J = j + y;
+               int I = i + x + dotw/2;
+               int J = j + y + doth/2;
                if (mask[dx][dy])
                   image[I][J] = dot_map[x][y];
                else
-                  image[I][J] = 0;
+                  image[I][J] = white;
             }
          }
       }
@@ -184,4 +218,6 @@ int main()
       cout << '\n';
       cerr << "Pixel row " << (i + 1) << " Written, of " << W << " rows." << '\n';
    }
+
+   print_consts();
 }
